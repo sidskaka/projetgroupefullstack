e11d253bff410a588f380fa973d5bafbfd9a3a4a
cyril-bonneau <cyril.bonneau@ynov.com>
2021-01-28 17:57:55 +0100

    toto

--- admin_panel/back/src/routes/user.route.js
+++ admin_panel/back/src/routes/user.route.js
@@ -8,23 +8,6 @@ const verifyToken = require('../helpers/verifyToken');
 const auth = require('../middleware/auth');
 
 
-//*******************employe Routers**************/
-// router.post('/employe', employe.create);
-// router.get('/employe', employe.findAll);
-// router.get('/employe/:id', employe.findOne);
-// router.post('/employe/:id', employe.updateOne);
-// router.put('/employe/:id', employe.updateOne);
-// router.delete('/employe/:id', employe.deleteOne);
-
-//*******************gestionnaire Routers**************/
-/*router.post('/gestionnaire', gestionnaire.create);
-router.get('/gestionnaire', gestionnaire.findAll);
-router.get('/gestionnaire/:id', gestionnaire.findOne);
-router.post('/gestionnaire/:id', gestionnaire.updateOne);
-router.put('/gestionnaire/:id', gestionnaire.updateOne);
-router.delete('/gestionnaire/:id', gestionnaire.deleteOne);*/
-
-
 //*****************Product Routers ********************/
 router.post('/product', Product.create);
 router.get('/products', Product.findAll);
