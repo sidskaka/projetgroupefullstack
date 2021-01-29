1ff03905e147f52c4277bb3135ea75bb952e20a9
sidskaka <sidyka01@gmail.com>
2021-01-29 11:01:08 +0100

    sids commit

--- admin_panel/admininstrationproject/src/App.js
+++ admin_panel/admininstrationproject/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Header from './Header';
 import Menu from './Menu';
@@ -8,19 +8,19 @@ import Form from './form';
 import Footer from './Footer';
 
 function App() {
-  return (
-    <Router>
-        <div class="wrapper">
-          <Header/>
-          <Menu/>
-            <Switch>
-                  <Route exact path='/' component={Dashboard} />
-                  <Route path='/form' component={Form} />
-            </Switch>
-          <Footer/>
-        </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div class="wrapper">
+                <Header />
+                <Menu />
+                <Switch>
+                    <Route exact path='/' component={Dashboard} />
+                    <Route path='/form' component={Form} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
